d22a72982d79e14f1090c43558341ddf84507d4f
urikoelk <95130767+urikoelk@users.noreply.github.com>
2022-01-07 02:58:34 +0200

    added basic tests for UnionFind and HashTable

--- main1.cpp
+++ main1.cpp
@@ -1,28 +1,44 @@
 #include <iostream>
 #include "UnionFind.h"
+#include "HashTable.h"
+
+class testObj { //has getSet method
+private:
+    int set;
+public:
+    int getSet() {
+        return set;
+    }
+    testObj(int a): set(a) {};
+    testObj() = default;
+};
 
 int main() {
-    int membersArr[] = {0,1,2};
-    int newSet, oldSet;
-    int set, member = 2;
-    UnionFind<int, int*> sets = UnionFind<int, int*>(3, membersArr);
+    HashTable<testObj> ht = HashTable<testObj>();
+    UnionFind<testObj, HashTable<testObj>> sets = UnionFind<testObj, HashTable<testObj>>(5, ht);
+    //put stuff in hash table
+    for (int i = 0; i < 15; ++i) {
+        ht.add(i,testObj(i));
+    }
+    ht.print();
+    //play with union find
     sets.print();
-
-    set = sets.find(member);
-    std::cout << "member " << member << " is in set " << set << std::endl;
+    int memberID = 2;
+    testObj set, newSet, oldSet;
+    set = sets.find(memberID);
+    std::cout << "member " << memberID << " is in set " << set.getSet() << std::endl;
 
     std::cout << "+++++" <<std::endl;
     sets.Union(0,1, &newSet, &oldSet);
     sets.print();
-    std::cout << "new set is: " << newSet << " old set is: " << oldSet << std::endl;
+    std::cout << "new set is: " << newSet.getSet() << " old set is: " << oldSet.getSet() << std::endl;
     std::cout << "+++++" <<std::endl;
     sets.Union(0,2, &newSet, &oldSet);
     sets.print();
-    std::cout << "new set is: " << newSet << " old set is: " << oldSet << std::endl;
+    std::cout << "new set is: " << newSet.getSet() << " old set is: " << oldSet.getSet() << std::endl;
     std::cout << "+++++" <<std::endl;
 
-    set = sets.find(member);
-    std::cout << "member " << member << " is in set " << set << std::endl;
-
-    return 1;
+    set = sets.find(memberID);
+    std::cout << "member " << memberID << " is in set " << set.getSet() << std::endl;
+     return 1;
 }
